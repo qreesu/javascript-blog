2f2c83921902c4b54dbc4fc504ac29f7af19241e
Krzysztof Tylka <krzysztof.tylka@ttpsc.pl>
2019-12-20 08:50:19 +0100

    Add remove class handler

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const titleClickHandler = function(event){
+const titleClickHandler = function (event) {
     console.log('Link was clicked!');
     console.log(event);
 
     /* remove class 'active' from all article links  */
+    removeClass(document.querySelectorAll('.titles a.active'), 'active')
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    removeClass(document.querySelectorAll('article.active'), 'active')
 
     /* get 'href' attribute from the clicked link */
 
@@ -19,6 +21,12 @@ const titleClickHandler = function(event){
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
+}
+
+const removeClass = function (objects, clazz) {
+    for (let object of objects) {
+        object.classList.remove(clazz)
+    }
 }
