671ef5f3647426f6f124268f441be9cace37ef13
Krzysztof Tylka <krzysztof.tylka@ttpsc.pl>
2019-12-22 21:27:34 +0100

    Finish titleClickHandler

--- js/script.js
+++ js/script.js
@@ -4,23 +4,25 @@ const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
     console.log('clickedElement', clickedElement);
 
     /* remove class 'active' from all article links  */
-    removeClass(document.querySelectorAll('.titles a.active'), 'active')
+    removeClass(document.querySelectorAll('.titles a.active'), 'active');
 
     /* add class 'active' to the clicked link */
     clickedElement.classList.add('active');
 
     /* remove class 'active' from all articles */
-    removeClass(document.querySelectorAll('article.active'), 'active')
+    removeClass(document.querySelectorAll('article.active'), 'active');
 
     /* get 'href' attribute from the clicked link */
+    const hrefAttributeValueOfClickedLink = this.getAttribute('href').substring(1);
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const articleRelatedToClickedLink = document.getElementById(hrefAttributeValueOfClickedLink);
 
     /* add class 'active' to the correct article */
+    articleRelatedToClickedLink.classList.add('active')
 }
 
 const links = document.querySelectorAll('.titles a');
